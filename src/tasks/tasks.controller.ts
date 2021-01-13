c6fc9adf63c4d7b1eae3f997cceb52c77b898c06
Pınar <pinarsuvac@gmail.com>
2021-01-13 20:09:34 +0300

    Crud operation add

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
-@Controller('tasks')
+@Controller('/tasks')
 export class TasksController {
     constructor(private tasksService: TasksService){}
 
     @Get()
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task{
+        return this.tasksService.getTaskById(id);
+    }
+    // First option for retrieve the request parameters from body
+    // @Post()
+    // createTask(@Body() body){
+    //     console.log('body', body);
+    // }
+
+    //Second Option for retrive the request parameters from body
+    // @Post()
+    // createTask(
+    //     @Body('title') title : string,
+    //     @Body('description') description: string
+    // ): Task{ // return type is task
+    //     return this.tasksService.createTask(title,description);
+    // }
+
+    // using DTO for retrive the data
+    @Post()
+    createTask(@Body() CreateTaskDto : CreateTaskDto){
+        return this.tasksService.createTask(CreateTaskDto);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id:string): void{
+        this.tasksService.deleteTask(id);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,41 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { v1 as uuidv1 } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = []; // task interfaceini model olarak alır
+    // tooks a task interface as a model
+    private tasks: Task[] = []; 
 
-    getAllTasks(): Task[] {  // task interfaceini model olarak alır
+    // tooks a task interface as a model. gets all tasks
+    getAllTasks(): Task[] {  
         return this.tasks;
+    }
+
+    // Retrive a spesific task
+    getTaskById(id:string) : Task{
+        return this.tasks.find(task => task.id === id);
+    }
+
+    // Creating a new task
+    createTask(CreateTaskDto): Task{
+
+        const {title, description} = CreateTaskDto;
+
+        const task: Task = {
+            id: uuidv1(),
+            title,  // same syntax with title : title, 
+            description,
+            status: TaskStatus.OPEN,
+        };
+
+        this.tasks.push(task);
+        return task;
+    }
+
+    deleteTask(id:string): void{
+        this.tasks = this.tasks.filter(task => task.id !== id);
+        
     }
 }
